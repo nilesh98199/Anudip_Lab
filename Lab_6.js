fbadb1697c736dbb8e3b7a8bd6490de5838b78ef
nilesh98199 <project.nilesh24@gmail.com>
2025-09-09 13:57:21 +0530

    Add files via upload

---
+++ Lab_6.js
@@ -0,0 +1,23 @@
+// Question :  Write a Node.js program that displays two separate messages in the console: 
+// ● "Hello, this is my first Node.js program!" 
+// ● "I am excited to learn more about Node.js!" 
+// Then, modify the program to concatenate these two messages into a single line and display: 
+
+let a = "Hello, this is my first Node.js program!";
+let b = "I am excited to learn more about Node.js!";
+let c = a + b;
+console.log(a);
+console.log(b);
+
+console.log(c);
+
+// OUTPUT
+// Hello, this is my first Node.js program!
+// I am excited to learn more about Node.js!
+// Hello, this is my first Node.js program!I am excited to learn more about Node.js!
+
+
+
+
+
+
